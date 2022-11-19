b100c67b9f72bba49004ea63faa69e0dc09fc285
zhudayang <zhudayang@sensetime.com>
2022-11-19 23:51:05 +0800

    init project

---
+++ src/main/java/com/wustzdy/springboot/elasticsearch/bean/ElasticsearchController.java
@@ -0,0 +1,14 @@
+package com.wustzdy.springboot.elasticsearch.bean;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * @author
+ * @Description
+ * @project springboot_learn
+ */
+@RestController
+public class ElasticsearchController {
+}
