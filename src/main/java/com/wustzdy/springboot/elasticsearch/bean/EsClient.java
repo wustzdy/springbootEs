f1312a19ad7d7d25ba01f29199b77dd6547eaefc
zhudayang <zhudayang@sensetime.com>
2022-11-20 00:02:37 +0800

    add EsClient

---
+++ src/main/java/com/wustzdy/springboot/elasticsearch/bean/EsClient.java
@@ -0,0 +1,20 @@
+package com.wustzdy.springboot.elasticsearch.bean;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EsClient {
+    @Bean
+    public RestHighLevelClient restHighLevelClient() {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        //若有多个，可以传一个数组
+                        new HttpHost("192.168.1.5", 9200, "http")));
+        System.out.println("connect success");
+        return client;
+    }
+}
