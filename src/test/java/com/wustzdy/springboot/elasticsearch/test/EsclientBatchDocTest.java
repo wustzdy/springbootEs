355cfd15270cc43540d4f3dc92008e30b028a215
zhudayang <zhudayang@sensetime.com>
2022-11-20 11:42:02 +0800

    批量添加文档

---
+++ src/test/java/com/wustzdy/springboot/elasticsearch/test/EsclientBatchDocTest.java
@@ -0,0 +1,55 @@
+package com.wustzdy.springboot.elasticsearch.test;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wustzdy.springboot.elasticsearch.SpringBootElasticsearchApplication;
+import com.wustzdy.springboot.elasticsearch.bean.EsClient;
+import com.wustzdy.springboot.elasticsearch.bean.Person;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootElasticsearchApplication.class)
+public class EsclientBatchDocTest {
+    @Autowired
+    private EsClient esClient;
+    String index = "person";
+    String type = "man";
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void createBatchDoc() throws IOException {
+
+        Person person1 = new Person(1, "张三", 23, new Date());
+        Person person2 = new Person(2, "李四", 24, new Date());
+        Person person3 = new Person(3, "王武", 25, new Date());
+
+        String json1 = objectMapper.writeValueAsString(person1);
+        String json2 = objectMapper.writeValueAsString(person2);
+        String json3 = objectMapper.writeValueAsString(person3);
+
+        BulkRequest bulkRequest = new BulkRequest();
+        bulkRequest.add(new IndexRequest(index, type, person1.getId().toString()).source(json1, XContentType.JSON));
+        bulkRequest.add(new IndexRequest(index, type, person2.getId().toString()).source(json2, XContentType.JSON));
+        bulkRequest.add(new IndexRequest(index, type, person3.getId().toString()).source(json3, XContentType.JSON));
+
+        RestHighLevelClient client = esClient.restHighLevelClient();
+        BulkResponse response = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println("bulk添加文档：" + response.toString());
+
+    }
+}
