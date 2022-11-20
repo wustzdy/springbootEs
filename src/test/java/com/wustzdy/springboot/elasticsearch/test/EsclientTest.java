eee87cc8fc95d8c935ef6f157909b1b4bf5e0695
zhudayang <zhudayang@sensetime.com>
2022-11-20 10:05:43 +0800

    create index struct

--- src/test/java/com/wustzdy/springboot/elasticsearch/test/EsclientTest.java
+++ src/test/java/com/wustzdy/springboot/elasticsearch/test/EsclientTest.java
@@ -1,28 +1,36 @@
 package com.wustzdy.springboot.elasticsearch.test;
 
+import com.wustzdy.springboot.elasticsearch.SpringBootElasticsearchApplication;
 import com.wustzdy.springboot.elasticsearch.bean.EsClient;
 import org.elasticsearch.action.admin.indices.create.CreateIndexRequest;
 import org.elasticsearch.action.admin.indices.create.CreateIndexResponse;
 import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.json.JsonXContent;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = SpringBootElasticsearchApplication.class)
 public class EsclientTest {
 
     @Autowired
     private EsClient esClient;
+    String index="person";
+    String type="main";
 
     @Test
     public void createIndex() throws IOException {
         Settings.Builder settings = Settings.builder().put("number_of_replicas", 1).put("number_of_shards", 5);
         //关于准备索引的结构的aappings
-        JsonXContent.contentBuilder()
+        XContentBuilder mappings = JsonXContent.contentBuilder()
                 .startObject()
                     .startObject("properties")
                         .startObject("name")
@@ -31,15 +39,18 @@ public class EsclientTest {
                         .startObject("age")
                             .field("type","integer")
                         .endObject()
-                        .startObject("age")
+                        .startObject("birthday")
                              .field("type","date")
                              .field("format","yyyy-MM-dd")
                         .endObject()
                     .endObject()
                 .endObject();
 
-        CreateIndexRequest request = new CreateIndexRequest("xk_index");//索引名
-        CreateIndexResponse response = esClient.indices().create(request, RequestOptions.DEFAULT);
-        System.out.println("创建成功，创建的索引名为：" + response.index());
+        CreateIndexRequest request = new CreateIndexRequest(index)
+                .settings(settings)
+                .mapping(type,mappings);
+        RestHighLevelClient client = esClient.restHighLevelClient();
+        CreateIndexResponse response = client.indices().create(request, RequestOptions.DEFAULT);
+        System.out.println("创建成功，创建的索引名为：" + response.toString());
     }
 }
